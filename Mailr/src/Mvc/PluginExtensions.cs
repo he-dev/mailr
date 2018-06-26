8fd97687e0cae592a09782cec7822ae3cf6eecaf
ᎬᏁᎶᎥᏁᎬᎬᏒ <bennyboss+github@gmail.com>
2018-06-26 17:45:09 +0200

    clean-up

--- Mailr/src/Mvc/PluginExtensions.cs
+++ Mailr/src/Mvc/PluginExtensions.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Custom;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using JetBrains.Annotations;
 using Mailr.Mvc.Razor.ViewLocationExpanders;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
@@ -29,47 +31,31 @@ namespace Mailr.Mvc
 
             //logger.Log(Abstraction.Layer.Infrastructure().Variable(new { pluginAssemblies = extAssemblies.Select(x => x.FullName) }));
 
+            var extensionDirectories =
+                (hostingEnvironment.IsDevelopmentExt()
+                    ? EnumerateExtensionProjectDirectories(serviceProvider)
+                    : EnumerateExtensionInstallationDirectories(serviceProvider)).ToList();
+
             mvc
                 .ConfigureApplicationPartManager(apm =>
                 {
-                    //apm.ApplicationParts.Add(new AssemblyPart(typeof(Mailr.Extensions.Gunter.Controllers.RunTestController).Assembly));
-                    //return;
-                    var extensionDirectories =
-                        hostingEnvironment.IsDevelopmentExt()
-                            ? GetDevelopmentExtensionDirectories(serviceProvider)
-                            : GetExtensionDirectories(serviceProvider);
-                    //if (!configuration.GetSection("Extensibility:Enabled").Get<bool>())
-                    //{
-                    //    return;
-                    //}
                     foreach (var extensionDirectory in extensionDirectories)
                     {
-                        try
+                        if (TryLoadExtensionAssembly(serviceProvider, extensionDirectory, out var extensionAssembly))
                         {
-                            var extensionAssembly = LoadExtensionAssembly(serviceProvider, extensionDirectory);
                             logger.Log(Abstraction.Layer.Infrastructure().Meta(new { extensionAssembly = new { extensionAssembly.FullName } }));
                             apm.ApplicationParts.Add(new AssemblyPart(extensionAssembly));
-                        }
-                        catch (FileNotFoundException ex)
-                        {
-                            logger.Log(Abstraction.Layer.Infrastructure().Routine(nameof(LoadExtensionAssembly)).Faulted(), ex);
-                        }
-                        catch (ReflectionTypeLoadException ex)
-                        {
-                            logger.Log(Abstraction.Layer.Infrastructure().Routine(nameof(LoadExtensionAssembly)).Faulted(), ex);
-                        }
-                        catch (Exception ex)
-                        {
-                            logger.Log(Abstraction.Layer.Infrastructure().Routine($"{nameof(AssemblyPart)}.ctor").Faulted(), ex);
                         }
                     }
                 });
 
-            ConfigureAssemblyResolve(serviceProvider);
-
+            if (hostingEnvironment.IsProduction() || hostingEnvironment.IsDevelopment())
+            {
+                ConfigureAssemblyResolve(serviceProvider);
+            }
 
             var resouceFileProviders =
-                EnumerateResourceDirectories(serviceProvider)
+                extensionDirectories
                     .Select(path => new PhysicalFileProvider(path))
                     .ToList();
 
@@ -78,10 +64,7 @@ namespace Mailr.Mvc
 
             mvc
                 .Services
-                .AddSingleton<IFileProvider>(fileProvider);
-
-            mvc
-                .Services
+                .AddSingleton<IFileProvider>(fileProvider)
                 .Configure<RazorViewEngineOptions>(options =>
                 {
                     options
@@ -89,32 +72,48 @@ namespace Mailr.Mvc
                         .Add(fileProvider);
                 });
 
-
             return mvc;
         }
 
-        private static IEnumerable<string> GetExtensionDirectories(IServiceProvider serviceProvider)
+        private static IEnumerable<string> EnumerateExtensionInstallationDirectories(IServiceProvider serviceProvider)
         {
             var configuration = serviceProvider.GetService<IConfiguration>();
             var hostingEnvironment = serviceProvider.GetService<IHostingEnvironment>();
 
+            // Razor view engine requires this path too.
             var extRootPath = Path.Combine(hostingEnvironment.ContentRootPath, configuration["Extensibility:Ext"]);
 
             return
                 Directory.Exists(extRootPath)
-                    ? Directory.GetDirectories(extRootPath)
+                    ? Directory.GetDirectories(extRootPath).Prepend(extRootPath)
                     : Enumerable.Empty<string>();
         }
 
-        private static Assembly LoadExtensionAssembly(IServiceProvider serviceProvider, string extensionDirectory)
+        private static IEnumerable<string> EnumerateExtensionProjectDirectories(IServiceProvider serviceProvider)
+        {
+            var configuration = serviceProvider.GetService<IConfiguration>();
+            var hostingEnvironment = serviceProvider.GetService<IHostingEnvironment>();
+
+            // Extension development requires them to be referenced as projects.
+
+            // This path is required to find static files by css-provider.
+            var solutionExtensions = configuration["Extensibility:Development:SolutionDirectory"];
+            var projectNames = configuration.GetSection("Extensibility:Development:ProjectNames").GetChildren().AsEnumerable().Select(x => x.Value);
+
+            //foreach (var directory in Directory.EnumerateDirectories(extensionsRootDirectory.FullName, "Mailr.Extensions.*"))
+            foreach (var directory in projectNames.Prepend(solutionExtensions).Select(projectName => Path.Combine(solutionExtensions, projectName)))
+            {
+                yield return directory;
+            }
+        }
+
+        private static bool TryLoadExtensionAssembly(IServiceProvider serviceProvider, string extensionDirectory, out Assembly assembly)
         {
             var hostingEnvironment = serviceProvider.GetService<IHostingEnvironment>();
             var configuration = serviceProvider.GetService<IConfiguration>();
+            var logger = serviceProvider.GetService<ILoggerFactory>().CreateLogger<Startup>();
 
-            var binDirectoryName = 
-                hostingEnvironment.IsDevelopmentExt()
-                    ? @"bin\Debug\net47\win81-x64"
-                    : configuration["Extensibility:Bin"];
+            var binDirectoryName = configuration["Extensibility:Bin"];
 
             var extensionName = Path.GetFileName(extensionDirectory);
 
@@ -131,60 +130,21 @@ namespace Mailr.Mvc
                     $"{extensionName}.dll"
                 );
 
-            return
-                File.Exists(extensionDllName)
-                    ? Assembly.LoadFile(extensionDllName)
-                    : throw new FileNotFoundException($"{extensionDllName} not found.");
-        }
-
-        private static IEnumerable<Assembly> GetExtensionAssemblies(IServiceProvider serviceProvider)
-        {
-            var configuration = serviceProvider.GetService<IConfiguration>();
-            var hostingEnvironment = serviceProvider.GetService<IHostingEnvironment>();
-            var logger = serviceProvider.GetService<ILoggerFactory>().CreateLogger<Startup>();
-
-            if (hostingEnvironment.IsDevelopmentExt())
+            try
             {
-                yield break;
-            }
-
-            var extRootPath = Path.Combine(hostingEnvironment.ContentRootPath, configuration["Extensibility:Ext"]);
-            var binDirectoryName = configuration["Extensibility:Bin"];
-
-            if (!Directory.Exists(extRootPath))
-            {
-                yield break;
-            }
-
-            var pluginDirectories = Directory.GetDirectories(extRootPath);
-            foreach (var pluginDirectory in pluginDirectories)
-            {
-                // Extension assemblies are located in the {Extensibility:Ext}: ..\ext\Foo\bin\Foo.dll
-                var extFullName =
-                    Path.Combine(
-                        pluginDirectory,
-                        binDirectoryName,
-                        $"{Path.GetFileName(pluginDirectory)}.dll"
-                    );
-
-                if (File.Exists(extFullName))
+                if (File.Exists(extensionDllName))
                 {
-                    var extAssembly = default(Assembly);
-                    try
-                    {
-                        extAssembly = Assembly.LoadFile(extFullName);
-                    }
-                    catch (ReflectionTypeLoadException ex)
-                    {
-                        logger.Log(Abstraction.Layer.Infrastructure().Routine(nameof(Assembly.LoadFile)).Faulted(), ex);
-                    }
-
-                    if (!(extAssembly is null))
-                    {
-                        yield return extAssembly;
-                    }
+                    assembly = Assembly.LoadFile(extensionDllName);
+                    return true;
                 }
             }
+            catch (Exception ex)
+            {
+                logger.Log(Abstraction.Layer.Infrastructure().Routine(nameof(TryLoadExtensionAssembly)).Faulted(), ex);
+            }
+
+            assembly = default;
+            return false;
         }
 
         private static void ConfigureAssemblyResolve(IServiceProvider serviceProvider)
@@ -202,7 +162,7 @@ namespace Mailr.Mvc
                 // FooPlugin.FooClass, Version = 1.0.0.0, Culture = neutral, PublicKeyToken = null
                 var extDependencyName = e.Name.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).First();
 
-                // C:\..\ext\Plugin\bin\PluginDependency.dll
+                // C:\..\ext\Foo\bin\FooDependency.dll
                 var extDependencyFullName =
                     Path.Combine(
                         extRootPath,
@@ -218,60 +178,6 @@ namespace Mailr.Mvc
                         ? Assembly.LoadFile(extDependencyFullName)
                         : null;
             };
-        }
-
-        private static IEnumerable<string> EnumerateResourceDirectories(IServiceProvider serviceProvider)
-        {
-            var hostingEnvironment = serviceProvider.GetService<IHostingEnvironment>();
-            var configuration = serviceProvider.GetService<IConfiguration>();
-
-            // Razor view engine requires this path too.
-            yield return Path.Combine(hostingEnvironment.ContentRootPath, configuration["Extensibility:Ext"]);
-
-            // These paths are available when running as a service with installed plugins.
-            foreach (var extensionDirectory in GetExtensionDirectories(serviceProvider))
-            {
-                yield return extensionDirectory;
-            }
-
-            if (hostingEnvironment.IsDevelopmentExt())
-            {
-                // These paths are available when running in developmentExt with extension outside in the app folder.
-                foreach (var directory in GetDevelopmentExtensionDirectories(serviceProvider))
-                {
-                    yield return directory;
-                }
-            }
-        }
-
-        private static IEnumerable<string> GetDevelopmentExtensionDirectories(IServiceProvider serviceProvider)
-        {
-            var configuration = serviceProvider.GetService<IConfiguration>();
-            var hostingEnvironment = serviceProvider.GetService<IHostingEnvironment>();
-
-            // Extension development does not use plugins so we have to look for them in the current directory parent 
-            // because the service is "installed" as a submodule which is a subdirectory.
-
-            // ContentRootPath is the path of the *.csproj, we have to go back two levels to reach the extension directory.
-            //var extensionsRootDirectory =
-            //    new DirectoryInfo(hostingEnvironment.ContentRootPath).Parent?.Parent
-            //    ?? throw new DirectoryNotFoundException("Could not find extension directory.");
-
-            var solutionExtensions = configuration["Extensibility:Development:SolutionDirectory"];
-
-            yield return solutionExtensions;
-
-            var projectNames = configuration.GetSection("Extensibility:Development:ProjectNames").GetChildren().AsEnumerable().Select(x => x.Value);
-
-            // This path is required to find static files by css-provider.
-            //yield return extensionsRootDirectory.FullName;
-
-            ////return extensionsRootDirectory.FullName;
-            //foreach (var directory in Directory.EnumerateDirectories(extensionsRootDirectory.FullName, "Mailr.Extensions.*"))
-            foreach (var directory in projectNames.Select(projectName => Path.Combine(solutionExtensions, projectName)))
-            {
-                yield return directory;
-            }
         }
     }
 
